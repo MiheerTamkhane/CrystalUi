e1b58a566b16afa841369cf025e56337ad9555c4
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-02-08 18:19:00 +0530

    modal added in docs

---
+++ components/modal/modal.js
@@ -0,0 +1,14 @@
+const modalBtn = document.querySelector("#modal-btn");
+const ctModal = document.querySelector("#ct-modal");
+const ctModalParent = document.querySelector(".ct-modal-dialog");
+const modalClose = document.querySelector(".modal-close");
+
+modalBtn.addEventListener("click", () => {
+  ctModal.classList.add("ct-modal-active");
+  ctModalParent.classList.add("ct-dialog-active");
+});
+
+modalClose.addEventListener("click", () => {
+  ctModal.classList.remove("ct-modal-active");
+  ctModalParent.classList.remove("ct-dialog-active");
+});
