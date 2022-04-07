e503aea7ba5d65de4b0aaab106f0a5d90404ba41
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-04-07 16:48:27 +0530

    Update : Update the navbar hamburger/dropdown menu

---
+++ components/navbar/nav.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector(".ct-nav-hamburger");
+const ctNavSidebar = document.querySelector(".ct-nav-sidebar");
+const ctUserAcc = document.querySelector(".ct-nav-account");
+const person = document.querySelector(".person");
+let temp = false;
+hamburger.addEventListener("click", () => {
+  if (temp) {
+    ctNavSidebar.classList.add("show");
+    temp = false;
+  } else {
+    ctNavSidebar.classList.remove("show");
+    temp = true;
+  }
+});
+person.addEventListener("click", () => {
+  if (temp) {
+    ctUserAcc.classList.add("show");
+    temp = false;
+  } else {
+    ctUserAcc.classList.remove("show");
+    temp = true;
+  }
+});
